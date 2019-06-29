a93a3fddc6a2bb7734f29ef8a5bfc529dbde4d50
Alexander Sokolov <alexsokolovpro@gmail.com>
2019-06-29 18:10:48 +0300

    Добавил аватар пользователя. Страницу отображения профиля пользователя. Компонент для превью задачи - TaskPreview

---
+++ src/views/Task/TaskPreview/TaskPreview.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="task">
+        <div v-if="!isLoading" class="task__body"></div>
+        <div v-else class="task__body">
+            <v-progress-circular indeterminate color="primary"/>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "TaskPreview",
+    created() {
+      this.initTask();
+    },
+    data: () => ({
+      isLoading: true,
+      tasks: null
+    }),
+    computed: {
+      task () {
+        return false;
+      }
+    },
+    methods: {
+      async initTask () {
+        this.tasks = await fetch('http://10.20.2.65:8081/task/all');
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 5000);
+      }
+    }
+  }
+</script>
--- src/views/UserProfile/UserProfile.vue
+++ src/views/UserProfile/UserProfile.vue
@@ -1,9 +1,77 @@
 <template>
-    <div></div>
+    <div class="wrapper">
+        <v-container grid-list-md>
+            <v-layout row wrap>
+
+                <v-flex md4>
+                    <div v-if="user">
+                        <v-avatar :size="'128px'"
+                                  color="grey lighten-4 mb-3">
+                            <img src="../../assets/useravatar.jpg" alt="" style="max-width: 100%; height: auto" />
+                        </v-avatar>
+                        <h1 class="headline font-weight-black">{{ user.name }}</h1>
+                        <h2 class="subheading font-weight-normal mb-3">{{ user.post }}</h2>
+                        <h3><span>Rating:</span> {{ user.rating }}</h3>
+                        <v-list two-line>
+                            <template v-for="(language, index) in user.languages">
+                                <v-subheader :key="index">{{ language }}</v-subheader>
+                            </template>
+                        </v-list>
+                    </div>
+                </v-flex>
+
+                <v-flex md8>
+
+                    <v-expansion-panel>
+                        <v-expansion-panel-content v-for="(panel, index) in panelItems"
+                                                   :key="index">
+                            <template v-slot:header>
+                                <h3>{{ panel.name }}</h3>
+                            </template>
+
+                            <v-card>
+                                <v-card-text>
+                                    <TaskPreview />
+                                </v-card-text>
+                            </v-card>
+
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
 </template>
 
 <script>
+    import TaskPreview from '../Task/TaskPreview';
+
   export default {
-    name: "UserProfile"
+    name: "UserProfile",
+    components: {
+      TaskPreview
+    },
+    created() {
+      this.panelItems = [
+        {
+          name: 'My tasks',
+          expand: false
+        }
+      ];
+    },
+    data: () => ({
+      user: {
+        languages: [
+          'PHP',
+          'JavaScript',
+          'NodeJS',
+          'TypeScript',
+        ],
+        name: 'Sokolov Alexander',
+        post: 'Frontend developer',
+        rating: 4.6
+      }
+    }),
   }
 </script>
