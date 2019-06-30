82c2f48089f42704472a3152c284f7c88dad320f
Alexander Sokolov <alexsokolovpro@gmail.com>
2019-06-30 12:20:53 +0300

    Hiring

--- src/views/Hiring/Hiring.vue
+++ src/views/Hiring/Hiring.vue
@@ -1,78 +1,136 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-    <v-layout column class="align-center justify-center fill-height" style="min-height: calc(100vh - 124px)">
-        <span class="display-4 text-xs-center mt-5 pt-5" style="max-height: 20vh">Hire by<br><b>arbitrary</b><br>
-            criteria</span>
-        <v-layout row style="height: min-content" class="align-center">
-            <v-combobox
-                    v-model="model"
-                    :items="items"
-                    :search-input.sync="search"
-                    hide-selected
-                    multiple
-                    persistent-hint
-                    deletable-chips chips
-            >
-                <template v-slot:no-data>
-                    <v-list-tile>
-                        <v-list-tile-content>
-                            <v-list-tile-title>
-                                No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a
-                                new one
-                            </v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-            </v-combobox>
-            <v-btn icon large @click="search">
-                <v-icon>search</v-icon>
-            </v-btn>
-        </v-layout>
-        <v-card v-for="(u, index) of users" :key="index" style="max-width: 33vw">
-            <UserPreview :user="u"></UserPreview>
-        </v-card>
+    <v-container>
 
-    </v-layout>
+        <v-layout row class="align-center justify-center fill-height">
+            <v-flex class="py-5">
+                <span class="display-4 text-xs-center mt-5 pt-5">
+                    Hire by<br/>
+                    <strong>arbitrary</strong>
+                    <br/>
+                    criteria
+                </span>
+            </v-flex>
+        </v-layout>
+
+        <v-layout row>
+            <v-flex class="py-3">
+                <v-select v-model="selectedTags"
+                          :items="items"
+                          item-text="tag"
+                          item-value="{ tag, rating }"
+                          return-object
+                          chips
+                          multiple />
+            </v-flex>
+        </v-layout>
+
+        <v-layout row>
+            <v-layout>
+                <v-flex md12>
+                    <v-card  v-for="(m, index) in selectedTags" :key="index" class="mb-3">
+                        <div class="px-4 py-4">
+                            <h2>{{ m.tag }} ({{ m.rating }})</h2>
+                            <v-slider v-model="m.rating" class="mb-0 fluid" max="10" step="0.1" />
+                        </div>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+
+            <v-card v-for="(u, index) of users" :key="index" style="max-width: 33vw">
+                <UserPreview :user="u"></UserPreview>
+            </v-card>
+
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-    import axios from "axios";
-    import UserPreview from "../User/UserPreview/UserPreview";
+  import axios from "axios";
+  import UserPreview from "../User/UserPreview/UserPreview";
 
-    export default {
-        name: "Hiring",
-        components: {UserPreview},
-        data() {
-            return {
-                items: ['SQL', 'Design', 'Vue', 'Node.js', 'Python', 'Java', 'Data Science', 'Gamedev', 'Ruby',
-                    'Dart', 'UE4', 'Vim', 'Brainfuck'],
-                model: ['Node.js', 'Angular'],
-                users: [],
-            };
-        },
-        watch: {
-            model(val) {
-                if (val.length > 5) {
-                    this.$nextTick(() => this.model.pop())
-                }
-            }
-        },
-        methods: {
-            search() {
-                axios.post('http://10.20.2.65:8081/task/suitable?id=1', {
-                        filters: this.model
-                    },
-                    {
-                        'Access-Control-Allow-Origin': '*',
-                    },)
-                    .then(({data}) => {
-                        this.users = data;
-                    })
-                    .catch(error => {
-                        throw new Error(error);
-                    });
-            }
-        }
+  export default {
+    name: "Hiring",
+    components: {UserPreview},
+    data() {
+      return {
+        items: [
+          {
+            tag: 'SQL',
+            rating: 0
+          },
+          {
+            tag: 'Design',
+            rating: 0
+          },
+          {
+            tag: 'Vue',
+            rating: 0
+          },
+          {
+            tag: 'Node.js',
+            rating: 0
+          },
+          {
+            tag: 'Python',
+            rating: 0
+          },
+          {
+            tag: 'Java',
+            rating: 0
+          },
+          {
+            tag: 'Data Science',
+            rating: 0
+          },
+          {
+            tag: 'Gamedev',
+            rating: 0
+          },
+          {
+            tag: 'Ruby',
+            rating: 0
+          },
+          {
+            tag: 'Dart',
+            rating: 0
+          },
+          {
+            tag: 'UE4',
+            rating: 0
+          },
+          {
+            tag: 'Vim',
+            rating: 0
+          },
+          {
+            tag: 'Brainfuck',
+            rating: 0
+          },
+        ],
+        selectedTags: [{
+          tag: 'Brainfuck',
+          rating: 0
+        }],
+        users: [],
+      };
+    },
+    methods: {
+      search() {
+        axios.post('http://10.20.2.65:8081/task/suitable?id=1', {
+            filters: this.selectedTags
+          },
+          {
+            'Access-Control-Allow-Origin': '*',
+          },)
+        .then(({data}) => {
+          this.users = data;
+        })
+        .catch(error => {
+          throw new Error(error);
+        });
+      }
     }
+  }
 </script>
 
 <style scoped>
